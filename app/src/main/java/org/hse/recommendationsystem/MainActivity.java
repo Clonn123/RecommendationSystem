1d71a600e70caf132041114dbad19ba2536851d7
Poloznikov Artem <art-clon@mail.ru>
2024-06-19 01:13:01 +0500

    Пока не работает, но фундамент есть

--- app/src/main/java/org/hse/recommendationsystem/MainActivity.java
+++ app/src/main/java/org/hse/recommendationsystem/MainActivity.java
@@ -1,14 +1,61 @@
 package org.hse.recommendationsystem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case 1000007:
+                // Логика для открытия домашней страницы
+                fragment = new HomeFragment();
+                break;
+            case 1000001:
+                // Логика для открытия страницы аниме
+                fragment = new AnimeFragment();
+                break;
+            case 1000006:
+                // Логика для открытия страницы манга
+                fragment = new MangaFragment();
+                break;
+            // Добавьте остальные кейсы для элементов меню
+        }
+
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
