ba30c063e0c1d60ab710ab99a5e48f6c12dc4fcf
Poloznikov Artem <art-clon@mail.ru>
2024-06-19 20:50:00 +0500

    Фундамент связи с бэком

---
+++ app/src/main/java/org/hse/recommendationsystem/ApiClient.java
@@ -0,0 +1,19 @@
+package org.hse.recommendationsystem;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String BASE_URL = "http://127.0.0.1:8000/api/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/org/hse/recommendationsystem/ApiService.java
@@ -0,0 +1,16 @@
+package org.hse.recommendationsystem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Body;
+
+public interface ApiService {
+//    @GET("yourmodel/")
+//    Call<List<YourModel>> getYourModels();
+//
+//    @POST("yourmodel/")
+//    Call<YourModel> createYourModel(@Body YourModel yourModel);
+}
